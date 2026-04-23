a44fb22e42a537b5a67b97d573d98f6275829a26
Ronnie Smith-Drake <73966668+ronnn-ddr@users.noreply.github.com>
2026-04-23 17:18:32 +0100

    Update configuration for ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Budgetting App'
+copyright = '2026'
+author = 'Team 3B'
 
 release = '0.1'
 version = '0.1.0'
